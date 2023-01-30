7975725e6e4a8d9912aabf880d93e6f944da819e
sugandifs <stephanie.sugandi@gmail.com>
2023-01-29 18:45:05 -0600

    added navbar logo, fixed and animated navbar to solidify on scroll, deleted program.html and associated lines in views.py and urls.py, embedded a temporary yt video, changed fonts and animated cards on hover, testimonials are no longer a carousel but are quotes, added cool-background.png, iss.jpg, moon.jpg, orbital.jpg, also did other minor cosmetic changes to index.html

--- webmaster/odyssey/views.py
+++ webmaster/odyssey/views.py
@@ -4,20 +4,21 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
 
-#google sheets api packages
+# google sheets api packages
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
 # Create your views here.
 
-#gc = gspread.service_account(filename='odyssey/static/odyssey/credentials.json')
-#sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1GwTC2kRQuuncIsxlGKaz68ynEZmeo49tzSszGuxmD0E/")
+# gc = gspread.service_account(filename='odyssey/static/odyssey/credentials.json')
+# sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1GwTC2kRQuuncIsxlGKaz68ynEZmeo49tzSszGuxmD0E/")
 scope = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive',
 ]
-creds = ServiceAccountCredentials.from_json_keyfile_name('odyssey/static/odyssey/credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    'odyssey/static/odyssey/credentials.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open("tsa-webmaster").sheet1
 
@@ -30,17 +31,15 @@ def sources(request):
     return render(request, "odyssey/sources.html")
 
 
-def program(request):
-    return render(request, "odyssey/program.html")
-
-
 def register(request):
     return render(request, "odyssey/order.html")
 
-#Needs to update google spreadsheet
+# Needs to update google spreadsheet
+
 
 def isEmptyData(row):
-    return sheet.cell(row,1).value is None
+    return sheet.cell(row, 1).value is None
+
 
 def register_view(request):
     if request.method == "POST":
@@ -65,10 +64,11 @@ def register_view(request):
         while True:
             if isEmptyData(row):
                 break
-            row+=1
+            row += 1
         for i in info_arr:
-            sheet.update_cell(row,info_arr.index(i)+1,i)
-        messages.success(request, "Congratulations! You've successfully booked!")
+            sheet.update_cell(row, info_arr.index(i)+1, i)
+        messages.success(
+            request, "Congratulations! You've successfully booked!")
         return HttpResponseRedirect(reverse('register'))
     messages.error(request, "Unsuccessful, try again.")
     return HttpResponseRedirect(reverse('register'))
