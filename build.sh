41034571a76424365e090bc52ab384b873d38c4f
KhoiTran2023 <64371084+KhoiTran2023@users.noreply.github.com>
2023-02-01 12:33:37 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r webmaster/requirements.txt
 pip install gunicorn
-python manage.py collectstatic --no-input
-python manage.py migrate
+python webmaster/manage.py collectstatic --no-input
+python webmastermanage.py migrate
