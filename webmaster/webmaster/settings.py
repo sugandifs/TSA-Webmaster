26a8ba7a12b4c91ed0c1b47cc00a7ce29e81b590
Gilbert Khoi Tran <gilbertktran@Gilberts-MacBook-Air.local>
2023-02-01 09:51:40 -0600

    Changed allowed hosts to be all and debug to false to prep for deployment

--- webmaster/webmaster/settings.py
+++ webmaster/webmaster/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%n_k2+l3i6(9^2r+w0mne6wb6!d-9ft02rj9@t0w=)=ojcog74'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
